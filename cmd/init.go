eacabcc2babfbad453ffca5380910b1f886e2a8a
JohnKinyanjui <johnkinyanjuiwanjiru@gmail.com>
2024-02-21 00:57:05 +0300

    minor updates of decription

--- cmd/init.go
+++ cmd/init.go
@@ -11,13 +11,8 @@ import (
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create a config file to allow easy arrangement of migration folders, database and networks in your project",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		var migrator = app.Migrate()
 		migrator.InitConfig()
