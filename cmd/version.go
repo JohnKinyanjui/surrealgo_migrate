cd9f112c3574c8b6f14e1ddf1279de0f416521a0
JohnKinyanjui <johnkinyanjuiwanjiru@gmail.com>
2024-02-29 23:04:16 +0300

    version command changed

---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Check current cli version",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		var version = "v1.0.3"
+		fmt.Println("surrealgo_migration version: ", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
